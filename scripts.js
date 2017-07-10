3fedf7786dd7d682817850c2e3a34e84c6bc1a07
tsmilan <101035074@student.swin.edu.au>
2017-07-10 18:48:05 +1000

    add largestEl solution

--- scripts.js
+++ scripts.js
@@ -1,32 +1,39 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(array){
     // your code here
+    var max = array[0];
+    for (var i = 1; i <= array.length - 1; i++) {
+      if(array[i] > max) {
+         max = array[i];
+      }
+    }
+    return max;
   },
-  
-  reversed: function(){  
+
+  reversed: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(){
     // your code here
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
